fc2cf59277ffc5f9ba356f1affc8ea5c696b6a31
oliferovych <168459324+oliferovych@users.noreply.github.com>
2024-10-21 14:49:04 +0200

    final

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 12:06:39 by dolifero          #+#    #+#             */
-/*   Updated: 2024/10/15 12:09:50 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/10/16 15:14:29 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,39 @@
 
 class Bureaucrat
 {
-	
+	private:
+		std::string const _name;
+		int _grade;
+		void setGrade(int grade);
+	public:
+	//Constructros
+		Bureaucrat();
+		Bureaucrat(std::string const name, int grade);
+		Bureaucrat(const Bureaucrat &other);
+	//Operator "=" overload
+		Bureaucrat &operator=(const Bureaucrat &other);
+	//Deconstructor
+		~Bureaucrat();
+	//Getters
+		std::string getName() const;
+		int getGrade() const;
+	//Methods
+		void incrementGrade();
+		void decrementGrade();
+	//Exceptions
+	class GradeTooHighException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+	};
 };
+
+std::ostream	&operator<<(std::ostream &stream, const Bureaucrat &b);
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,55 @@
 /*   By: dolifero <dolifero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 12:06:11 by dolifero          #+#    #+#             */
-/*   Updated: 2024/10/15 12:06:13 by dolifero         ###   ########.fr       */
+/*   Updated: 2024/10/16 15:28:41 by dolifero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+int main(void)
+{
+	Bureaucrat b1("b1", 150);
+	Bureaucrat b2("b2", 1);
+	Bureaucrat b3("b3", 2);
+	Bureaucrat b4("b4", 149);
+	Bureaucrat b5("b5", 150);
+	Bureaucrat b6("b6", 1);
+	Bureaucrat b7("b7", 2);
+	Bureaucrat b8("b8", 149);
+	Bureaucrat b9("b9", 150);
+
+	try
+	{
+		b1.incrementGrade();
+	}
+	catch(const Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << b1 << e.what() << '\n';
+	}
+	try
+	{
+		b2.decrementGrade();
+	}
+	catch(const Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << b2 << e.what() << '\n';
+	}
+	try
+	{
+		b3.incrementGrade();
+	}
+	catch(std::exception &e)
+	{
+		std::cerr << b3 << e.what() << '\n';
+	}
+	try
+	{
+		b4.decrementGrade();
+	}
+	catch(std::exception &e)
+	{
+		std::cerr << b4 << e.what() << '\n';
+	}
+	std::cout << b1 << b2 << b3 << b4 << b5 << b6 << b7 << b8 << b9;
+}
